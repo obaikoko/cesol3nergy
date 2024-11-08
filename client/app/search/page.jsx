e7d123f5414e8753c30b039d6042ae1f56edbb34
obaikoko <jesseobinna7@gmail.com>
2024-11-08 20:12:02 +0100

    updated

--- client/app/search/page.jsx
+++ client/app/search/page.jsx
@@ -1,9 +1,11 @@
 'use client';
-import React from 'react';
+import { Suspense } from 'react';
+
 import { useSearchParams } from 'next/navigation';
 import { useSearchProductsQuery } from '@/src/slices/productApiSlice';
 import Products from '@/components/Products';
 import SearchBox from '@/components/SearchBox';
+import Spinner from '@/components/Spinner';
 
 const SearchPage = () => {
   const searchParams = useSearchParams();
@@ -26,4 +28,12 @@ const SearchPage = () => {
   );
 };
 
-export default SearchPage;
+const Search = () => {
+  return (
+    <Suspense fallback={<Spinner clip={true} size={150} />}>
+      <SearchPage />
+    </Suspense>
+  );
+};
+
+export default Search;
