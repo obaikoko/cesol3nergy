334fc51b3ee92ea5a7478800a64b877706966356
obaikoko <jesseobinna7@gmail.com>
2024-11-12 19:28:43 +0100

    updated cors

--- client/src/slices/apiSlice.js
+++ client/src/slices/apiSlice.js
@@ -1,8 +1,8 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: 'http://localhost:5000',
-  // baseUrl: 'https://cesol3nergy-server.onrender.com',
+  // baseUrl: 'http://localhost:5000',
+  baseUrl: 'https://cesol3nergy-server.onrender.com',
 });
 
 export const apiSlice = createApi({
