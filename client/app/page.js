2f6ccf7d0d6877930055adbbfbf4897364d6bb4c
obaikoko <jesseobinna7@gmail.com>
2025-01-15 23:45:14 +0100

    updated metadata

--- client/app/page.js
+++ client/app/page.js
@@ -9,6 +9,14 @@ import Testimonials from '@/components/Testimonials';
 import WhyChooseUs from '@/components/WhyChooseUs';
 import React from 'react';
 
+export const metadata = {
+  title: 'Cesol3nergy - Solar Products & Renewable Energy Solutions',
+  description:
+    'Cesol3nergy offers premium solar panels, inverters, batteries, and renewable energy solutions. Empower your home or business with clean energy today!',
+  keywords:
+    'solar products, renewable energy, solar panels, solar inverters, solar batteries, clean energy, Cesol3nergy',
+  image: '/images/logo.jpg',
+};
 const page = () => {
   return (
     <div>
